39ac96b7cc40bfa769cb33193780d8c995beca87
TarkWight <arsentiy.gonda@gmail.com>
2024-12-09 22:14:41 +0700

    Updated the matrix buffer

--- Valeria/hw2/main.c
+++ Valeria/hw2/main.c
@@ -4,28 +4,51 @@
 #include <time.h>
 
 // Константы
-const int SIZE = 10; // Размер игрового поля
-const char EMPTY = ' '; // Пустая клетка
-const char SHIP = 'S'; // Корабль
-const char HIT = 'X'; // Попадание
-const char MISS = '.'; // Промах
-const char BUFFER = '_'; // Для маски
+const int SIZE = 10;
+const char EMPTY = ' ';
+const char SHIP = 'S';
+const char HIT = 'X';
+const char MISS = '.';
+const char DESTROYED = 'D';
+const int BUFFER_LOCK_CELL = -1;
+const int BUFFER_EMPTY_CELL = 0;
+const int BUFFER_SHIP_CELL = 1;
 
 // Прототипы функций
-void initializeField(char field[SIZE][SIZE], char mask[SIZE][SIZE]);
-void printField(char field[SIZE][SIZE], int hideShips);
-void placeShipsManual(char field[SIZE][SIZE], char mask[SIZE][SIZE]);
-void placeShipsAuto(char field[SIZE][SIZE], char mask[SIZE][SIZE]);
-int isValidPlacement(char mask[SIZE][SIZE], int x, int y, int length, int horizontal);
-void addBufferZone(char mask[SIZE][SIZE], int x, int y, int length, int horizontal);
-int shoot(char field[SIZE][SIZE], char mask[SIZE][SIZE], int x, int y);
-void computerMove(char field[SIZE][SIZE], int *x, int *y);
-int checkWin(char field[SIZE][SIZE]);
 int parseCoordinates(char *input, int *x, int *y);
+int checkWin(char field[SIZE][SIZE]);
+
+int shoot(char field[SIZE][SIZE], int mask[SIZE][SIZE], int x, int y);
+int isShipSunk(char field[SIZE][SIZE], int x, int y);
+void placeShipsManual(char field[SIZE][SIZE], int mask[SIZE][SIZE]);
+void placeShipsAuto(char field[SIZE][SIZE], int mask[SIZE][SIZE]);
+void addBufferZone(int mask[SIZE][SIZE], int x, int y, int length, int horizontal);
+int isValidPlacement(int mask[SIZE][SIZE], int x, int y, int length, int horizontal);
+void initializeField(char field[SIZE][SIZE], int mask[SIZE][SIZE]);
+void printField(char field[SIZE][SIZE], int hideShips);
+void computerMoveEasy(char field[SIZE][SIZE], int *x, int *y);
+void computerMoveHard(char field[SIZE][SIZE], int *x, int *y);
+
+void printMask(int field[SIZE][SIZE], int isHideShips) {
+    printf("   ");
+    for (int i = 1; i <= SIZE; i++) printf("%d ", i);
+    printf("\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%c  ", 'A' + i); // A-J for rows
+        for (int j = 0; j < SIZE; j++) {
+            printf("%d ", field[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 
 int main() {
-    char playerField[SIZE][SIZE], playerMask[SIZE][SIZE];
-    char computerField[SIZE][SIZE], computerMask[SIZE][SIZE];
+    char playerField[SIZE][SIZE];
+    int playerMask[SIZE][SIZE];
+
+    char computerField[SIZE][SIZE];
+    int computerMask[SIZE][SIZE];
     int x, y, playerWin = 0, computerWin = 0;
 
     srand((unsigned int)time(NULL));
@@ -45,11 +68,13 @@ int main() {
         // Ходы игрока
         printf("Your field:\n");
         printField(playerField, 0);
+        printMask(playerMask, 0);
         printf("Computer's field:\n");
         printField(computerField, 1);
 
         int playerTurn = 1; // Флаг хода игрока
         while (playerTurn && !playerWin) {
+
             printf("Enter coordinates (e.g., B5): ");
             char input[10];
             scanf("%s", input);
@@ -76,7 +101,7 @@ int main() {
         printf("Computer's turn...\n");
         int computerTurn = 1; // Флаг хода компьютера
         while (computerTurn && !computerWin) {
-            computerMove(playerField, &x, &y);
+            computerMoveEasy(playerField, &x, &y);
             printf("Computer shoots at (%c%d)\n", 'A' + x, y + 1);
             if (shoot(playerField, playerMask, x, y)) {
                 printf("Computer hit your ship! It gets another turn.\n");
@@ -101,35 +126,10 @@ int main() {
     return 0;
 }
 
-// Инициализация игрового поля
-void initializeField(char field[SIZE][SIZE], char mask[SIZE][SIZE]) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            field[i][j] = EMPTY;
-            mask[i][j] = EMPTY;
-        }
-    }
-}
 
-// Вывод игрового поля
-void printField(char field[SIZE][SIZE], int isHideShips) {
-    printf("   ");
-    for (int i = 1; i <= SIZE; i++) printf("%d ", i);
-    printf("\n");
-    for (int i = 0; i < SIZE; i++) {
-        printf("%c  ", 'A' + i); // A-J for rows
-        for (int j = 0; j < SIZE; j++) {
-            if (isHideShips && field[i][j] == SHIP)
-                printf("%c ", EMPTY);
-            else
-                printf("%c ", field[i][j]);
-        }
-        printf("\n");
-    }
-}
 
 // Размещение кораблей вручную
-void placeShipsManual(char field[SIZE][SIZE], char mask[SIZE][SIZE]) {
+void placeShipsManual(char field[SIZE][SIZE], int mask[SIZE][SIZE]) {
     int shipSizes[] = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1}; // Пример кораблей
     int x, y, horizontal;
 
@@ -157,7 +157,7 @@ void placeShipsManual(char field[SIZE][SIZE], char mask[SIZE][SIZE]) {
 }
 
 // Размещение кораблей компьютером
-void placeShipsAuto(char field[SIZE][SIZE], char mask[SIZE][SIZE]) {
+void placeShipsAuto(char field[SIZE][SIZE], int mask[SIZE][SIZE]) {
     int shipSizes[] = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
     int x, y, horizontal;
 
@@ -182,18 +182,20 @@ void placeShipsAuto(char field[SIZE][SIZE], char mask[SIZE][SIZE]) {
 }
 
 // Проверка корректности размещения корабля
-int isValidPlacement(char mask[SIZE][SIZE], int x, int y, int length, int horizontal) {
+int isValidPlacement(int mask[SIZE][SIZE], int x, int y, int length, int horizontal) {
     for (int i = 0; i < length; i++) {
         int nx = x + (horizontal ? 0 : i);
         int ny = y + (horizontal ? i : 0);
-        if (nx < 0 || nx >= SIZE || ny < 0 || ny >= SIZE || mask[nx][ny] != EMPTY)
+        if (nx < 0 || nx >= SIZE || ny < 0 || ny >= SIZE || mask[nx][ny] != BUFFER_EMPTY_CELL) {
             return 0;
+        }
     }
     return 1;
 }
 
+
 // Добавление буферной зоны вокруг корабля
-void addBufferZone(char mask[SIZE][SIZE], int x, int y, int length, int horizontal) {
+void addBufferZone(int mask[SIZE][SIZE], int x, int y, int length, int horizontal) {
     for (int i = 0; i < length; i++) {
         int nx = x + (horizontal ? 0 : i);
         int ny = y + (horizontal ? i : 0);
@@ -203,37 +205,16 @@ void addBufferZone(char mask[SIZE][SIZE], int x, int y, int length, int horizont
                 int bufferX = nx + dx;
                 int bufferY = ny + dy;
 
-                if (bufferX >= 0 && bufferX < SIZE && bufferY >= 0 && bufferY < SIZE) {
-                    if (mask[bufferX][bufferY] == EMPTY) {
-                        mask[bufferX][bufferY] = BUFFER;
-                    }
+                if (bufferX >= 0 && bufferX < SIZE &&
+                    bufferY >= 0 && bufferY < SIZE &&
+                    mask[bufferX][bufferY] == BUFFER_EMPTY_CELL) {
+                    mask[bufferX][bufferY] = BUFFER_LOCK_CELL;
                 }
             }
         }
     }
 }
 
-// Обработка выстрела
-int shoot(char field[SIZE][SIZE], char mask[SIZE][SIZE], int x, int y) {
-    if (x < 0 || x >= SIZE || y < 0 || y >= SIZE)
-        return 0;
-
-    if (field[x][y] == SHIP) {
-        field[x][y] = HIT;
-        return 1;
-    } else if (field[x][y] == EMPTY) {
-        field[x][y] = MISS;
-    }
-    return 0;
-}
-
-// Ход компьютера
-void computerMove(char field[SIZE][SIZE], int *x, int *y) {
-    do {
-        *x = rand() % SIZE;
-        *y = rand() % SIZE;
-    } while (field[*x][*y] == HIT || field[*x][*y] == MISS);
-}
 
 // Проверка победы
 int checkWin(char field[SIZE][SIZE]) {
@@ -254,4 +235,51 @@ int parseCoordinates(char *input, int *x, int *y) {
     *x = input[0] - 'A';
     *y = atoi(&input[1]) - 1;
     return (*x >= 0 && *x < SIZE && *y >= 0 && *y < SIZE);
+}
+
+// Обработка выстрела
+int shoot(char field[SIZE][SIZE], int mask[SIZE][SIZE], int x, int y) {
+    if (x < 0 || x >= SIZE || y < 0 || y >= SIZE)
+        return 0;
+
+    if (field[x][y] == SHIP) {
+        field[x][y] = HIT;
+        return 1;
+    } else if (field[x][y] == EMPTY) {
+        field[x][y] = MISS;
+    }
+    return 0;
+}
+// Инициализация игрового поля
+void initializeField(char field[SIZE][SIZE], int mask[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            field[i][j] = EMPTY;
+            mask[i][j] = BUFFER_EMPTY_CELL;
+        }
+    }
+}
+
+// Вывод игрового поля
+void printField(char field[SIZE][SIZE], int isHideShips) {
+    printf("   ");
+    for (int i = 1; i <= SIZE; i++) printf("%d ", i);
+    printf("\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%c  ", 'A' + i); // A-J for rows
+        for (int j = 0; j < SIZE; j++) {
+            if (isHideShips && field[i][j] == SHIP)
+                printf("%c ", EMPTY);
+            else
+                printf("%c ", field[i][j]);
+        }
+        printf("\n");
+    }
+}
+// Ход компьютера
+void computerMoveEasy(char field[SIZE][SIZE], int *x, int *y) {
+    do {
+        *x = rand() % SIZE;
+        *y = rand() % SIZE;
+    } while (field[*x][*y] == HIT || field[*x][*y] == MISS);
 }
